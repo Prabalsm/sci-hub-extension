d480208db87a5bf98eb313bebd7cde98e5862d9a
Prabal Singh Maharjn <prabalsmaharjan@gmail.com>
2021-07-25 16:47:08 +0545

    Initial commit

---
+++ popup.js
@@ -0,0 +1,38 @@
+
+let ValeContainer = document.getElementById("content");
+
+chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    let url = tabs[0].url;
+    ValeContainer.value = url;
+});
+
+document.querySelector('form').addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    var formData = {
+        url: ValeContainer.value
+    };
+    document.getElementById('requesting').innerHTML= "Requesting.....";
+    fetch('http://localhost:5000/extension', {
+        method: 'POST',
+        body: ValeContainer.value
+    })
+    .then(function (res) {
+        console.log(res);
+        if (res.status == 500){
+            throw new Error("Server Error");
+        }else if (res.status== 400){
+            throw new Error("Format Error");}  
+        return res.json();  
+    })
+    .then(function (json) {
+        console.log(json);
+       document.getElementById("requesting").innerHTML ="Completed";
+        setTimeout(() => {
+            chrome.tabs.create({ url: json.url});  
+        }, 1000);
+    })
+    .catch(function(error){
+        document.getElementById("requesting").innerHTML =error.message;
+    });
+    
+})
