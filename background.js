1c22e51dd3bb684238d338a856fcfbc0fc569175
Prabal Singh Maharjn <prabalsmaharjan@gmail.com>
2021-08-04 22:25:42 +0545

    Error reporting fixed Messaging system changed disconnect method

--- background.js
+++ background.js
@@ -1,50 +1,59 @@
-
 var context_menu ={
     "id":"Menu",
     "title": "sci-Download",
     "contexts":["all"]
 };
 
-chrome.contextMenus.create(context_menu);
+chrome.contextMenus.removeAll(function() {
+    chrome.contextMenus.create(context_menu);
+    });
 
 chrome.contextMenus.onClicked.addListener((clickdata)=>{
-    var formData = {
-        url: clickdata.pageUrl
-    };
-    sendar("Requesting.....");
-    console.log("Requesting.....");
-    fetch('http://localhost:5000/extension', {
-        method: 'POST',
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body: JSON.stringify(formData)
-    })
-    .then(function (res) {
-        console.log(res);
-        if (res.status == 500){
-            throw new Error("Server Error");
-        }else if (res.status== 400){
-            throw new Error("Format Error");}  
-        return res.json();  
-    })
-    .then(function (json) {
-        console.log(json);
-        console.log("Completed");
-        sendar("Completed");
-        setTimeout(() => {
-            chrome.tabs.create({ url: json.url});  
-        }, 1000);
-    })
-    .catch(function(error){
-        console.log(error.message);
-        sendar(error.message)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        tabid = tabs[0].id;  
+        console.log(tabid);
+        
+        var port = chrome.tabs.connect(tabid, {name:"tester"}); 
+        var formData = {
+            url: clickdata.pageUrl
+        };
+        console.log("Requesting.....");
+        port.postMessage({mes:"Requesting....."});
+        fetch('http://localhost:5000/extension', {
+            method: 'POST',
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(function (res) {
+            console.log(res);
+            if (res.status == 500){
+                throw new Error("Server Error");
+            }else if (res.status== 400){
+                return res.json();
+            }
+            return res.json();  
+        })
+        .then(function (json) {
+            console.log(json);
+            if ("message" in json){
+                throw json; 
+            }
+            console.log("Completed");
+            port.postMessage({mes:"Completed"});
+            setTimeout(() => {
+                chrome.tabs.create({ url: json.url});  
+            }, 1000);
+        })
+        .catch(function(error){
+            console.log(error.message);
+            port.postMessage({mes:error.message});
+            port.onDisconnect.addListener(console.log("disconnected"));
+            
+        });
+        
     });
 });
 
-function sendar(value){
-    chrome.tabs.query({active:true, currentWindow:true},(tabs)=>{
-        chrome.tabs.sendMessage(tabs[0].id, value);
-    }
-    );
-}
+
--- contentScripts.js
+++ contentScripts.js
@@ -1,13 +1,26 @@
+console.log("fucker");
 
-chrome.runtime.onMessage.addListener(
-    function gotMessage(message, sender, sendResponse){
-        let element = document.createElement("div");
-        element.id = "inject-container";
-        element.innerHTML ="<div id='inner'>"+message+"</div>"
-        document.body.appendChild(element);
-        setTimeout(() => {
-            document.getElementById("inject-container").remove()
-        }, 1000);
-        
-        
-})
+function injector(){
+    let element = document.createElement("div");
+    element.id = "inject-container";
+    element.innerHTML ="<div id='inner'></div>";
+    document.body.appendChild(element);
+}
+chrome.runtime.onConnect.addListener(function(port){
+    console.log("connected", port);
+    injector();
+    port.onMessage.addListener(function(msg){
+        document.getElementById("inner").innerHTML=msg.mes;
+        if(msg.mes =="Failed to fetch"){
+            setTimeout(() => {
+            document.getElementById("inject-container").remove();
+            port.disconnect();
+            }, 1500);}
+        if(msg.mes =="Completed"){
+            setTimeout(() => {
+            document.getElementById("inject-container").remove();
+            port.disconnect();
+            }, 1500);}
+    });
+});
+
